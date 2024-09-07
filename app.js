c5dc4fff6f62aabe78fdc9c5b044af2dcd51f9fc
WDDilay <156734101+WDDilay@users.noreply.github.com>
2024-09-07 09:10:06 +0800

    changes

--- app.js
+++ app.js
@@ -4,18 +4,14 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Set up view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Use routes from the index.js file
 const indexRouter = require('./routes/index');
 app.use('/', indexRouter);
 
-// Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
--- routes/index.js
+++ routes/index.js
@@ -13,5 +13,20 @@ router.get('/viewbora', (req, res) => {
     res.render('viewbora', { title: 'Boracay Island White Beach' });
 });
 
+router.get('/viewbohol', (req, res) => {
+    res.render('viewbohol', { title: 'About Bohol' });
+});
+
+router.get('/viewpalawan', (req, res) => {
+    res.render('viewpalawan', { title: 'About Puerto Princesa Palawan' });
+});
+
+router.get('/viewcloud9', (req, res) => {
+    res.render('viewcloud9', { title: 'About Cloud 9 surfing spot on Siargao' });
+});
+
+router.get('/viewcebu', (req, res) => {
+    res.render('viewcebu', { title: 'About Diving SPot on Cebu Island' });
+});
 
 module.exports = router;
